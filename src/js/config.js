2a103e017bdd715b4395a88beee272ac06fe447b
TeghveerK <tkooner@uci.edu>
2022-11-28 19:59:44 -0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '995ed656-e391-4f8a-b819-e95dc6e9aa59';
+export const MODAL_CLOSE_SEC = 2.5;
